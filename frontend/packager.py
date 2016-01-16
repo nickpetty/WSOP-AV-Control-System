04683897c29ae11667efc46f497f55f3189340c6
Nicholas Petty <nick@ihackeverything.com>
2016-01-16 12:40:00 -0600

    changes

--- frontend/packager.py
+++ frontend/packager.py
@@ -2,4 +2,4 @@ from subprocess import call
 
 print 'Building....'
 
-call(['electron-packager','. WSOP --platform=win32 --arch=x64 --version=0.36.0 --overwrite'])
+call(['electron-packager . WSOP --platform=win32 --arch=x64 --version=0.36.0 --overwrite'])
